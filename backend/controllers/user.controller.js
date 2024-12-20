a8685c040911d763ff4fc5a62a6d87bfce6ebb37
Rahul Anwala <106584354+rahulanwal@users.noreply.github.com>
2024-12-20 15:13:49 +0530

    doit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -6,7 +6,7 @@ const getUsersForSidebar = async (req, res) => {
 
         const filteredUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
 
-        res.status(200).json(filteredUsers );
+        res.status(200).json(filteredUsers);
     }
     catch(error){
         console.log("Error in getUsersForSidebar controller: ", error.message);
